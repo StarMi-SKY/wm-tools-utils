33bdc366b4f03f3a71c0cc7d6c7ea65c62d827b1
TCCN_leijing <lei.jing@toyotaconnected.com.cn>
2024-08-26 18:51:57 +0800

    [feat]: first commit!!

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export { debounce } from './function/debounce.min' // 防抖
+export { throttle } from './function/throttle.min' // 节流
+export { deepClone } from './object/deepClone.min' // 深拷贝
+export { mergeObjects } from './object/mergeObjects.min' // 合并对象
+export { uniqueArray } from './array/uniqueArray.min' // 数组去重
+export { formatDate } from './currency/formatDate.min' // 格式化日期
+export { getRandomNumber, getRandomString } from './currency/randomNumberORString.min' // 获取随机数
+export { deepMerge } from './currency/deepMerge.min' // 深合并
+export { getType } from './currency/judgmentType.min' // 获取类型
+export { getQueryParams } from './currency/parameterAcquisition.min' // 获取url参数
+export { sortArray } from './array/sortArray.min' // 数组排序
+export { trimString } from './string/trimString.min' // 去除字符串前后空格
+export { isValidIDCard,isValidEmail,isValidPhoneNumber } from './currency/verificationAssistance.min' // 身份证、邮箱、手机号码校验
+export { generateUUID } from './currency/generateUUID.min' // 生成uuid
