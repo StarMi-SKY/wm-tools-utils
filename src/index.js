2ddace9e164bd5a84adf08439b7a2a0a00ff4de0
TCCN_leijing <lei.jing@toyotaconnected.com.cn>
2024-09-24 22:48:24 +0800

    [fix]: 预览修复

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export { throttle } from './function/throttle.min' // 节流
 export { deepClone } from './object/deepClone.min' // 深拷贝
 export { mergeObjects } from './object/mergeObjects.min' // 合并对象
 export { uniqueArray } from './array/uniqueArray.min' // 数组去重
-export { formatDate } from './currency/formatDate.min' // 格式化日期
+export { formatDate } from './date/formatDate.min' // 格式化日期
 export { getRandomNumber, getRandomString } from './currency/randomNumberORString.min' // 获取随机数
 export { deepMerge } from './currency/deepMerge.min' // 深合并
 export { getType } from './currency/judgmentType.min' // 获取类型
@@ -14,3 +14,4 @@ export { isValidIDCard,isValidEmail,isValidPhoneNumber } from './currency/verifi
 export { generateUUID } from './currency/generateUUID.min' // 生成uuid
 export { serializeFunction,deserializeFunction } from './function/functionSerialization.min' // 函数序列化与反序列化
 export { startUpdateCheck } from './function/autoUpdate.min' // 启动更新检查
+export { areDatesWithinRange } from './date/areDatesWithinRange.min' // 判断日期是否在范围内
